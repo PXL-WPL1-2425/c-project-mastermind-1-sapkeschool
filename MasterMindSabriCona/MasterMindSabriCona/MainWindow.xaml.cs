4510dae28556c25330a9958538a0ad06c876ba6e
32489 <32489@SAPKE>
2024-11-17 03:36:10 +0100

    linking checkbox.item with circle fill collor in code behind

--- MasterMindSabriCona/MasterMindSabriCona/MainWindow.xaml.cs
+++ MasterMindSabriCona/MasterMindSabriCona/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace MasterMindSabriCona
 {
@@ -11,11 +12,72 @@ namespace MasterMindSabriCona
         {
             InitializeComponent();
 
-           
+            ComboBox1.SelectionChanged += ComboBox1_SelectionChanged;
+            ComboBox2.SelectionChanged += ComboBox2_SelectionChanged;
+            ComboBox3.SelectionChanged += ComboBox3_SelectionChanged;
+            ComboBox4.SelectionChanged += ComboBox4_SelectionChanged;
+
         }
 
+        private void ComboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboBox1.SelectedItem is ComboBoxItem selectedItem)
+            {
+                var selectedColor = (selectedItem.Background as SolidColorBrush)?.Color;
+                if (selectedColor.HasValue)
+                {
+                    Ellipse1.Fill = new SolidColorBrush(selectedColor.Value);
+                    ComboBox1.Background = new SolidColorBrush(selectedColor.Value);
+                    ComboBox1.Foreground = new SolidColorBrush(Colors.Black);
+                }
+            }
+        }
+
+        private void ComboBox2_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboBox2.SelectedItem is ComboBoxItem selectedItem)
+            {
+                var selectedColor = (selectedItem.Background as SolidColorBrush)?.Color;
+                if (selectedColor.HasValue)
+                {
+                    Ellipse2.Fill = new SolidColorBrush(selectedColor.Value);
+                    ComboBox2.Background = new SolidColorBrush(selectedColor.Value);
+                    ComboBox2.Foreground = new SolidColorBrush(Colors.Black);
+                }
+            }
+        }
+
+        private void ComboBox3_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboBox3.SelectedItem is ComboBoxItem selectedItem)
+            {
+                var selectedColor = (selectedItem.Background as SolidColorBrush)?.Color;
+                if (selectedColor.HasValue)
+                {
+                    Ellipse3.Fill = new SolidColorBrush(selectedColor.Value);
+                    ComboBox3.Background = new SolidColorBrush(selectedColor.Value);
+                    ComboBox3.Foreground = new SolidColorBrush(Colors.Black);
+                }
+            }
+        }
+
+        private void ComboBox4_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboBox4.SelectedItem is ComboBoxItem selectedItem)
+            {
+                var selectedColor = (selectedItem.Background as SolidColorBrush)?.Color;
+                if (selectedColor.HasValue)
+                {
+                    Ellipse4.Fill = new SolidColorBrush(selectedColor.Value);
+                    ComboBox4.Background = new SolidColorBrush(selectedColor.Value);
+                    ComboBox4.Foreground = new SolidColorBrush(Colors.Black);
+                }
+            }
+        }
     }
 }
+
+
 
 
 
